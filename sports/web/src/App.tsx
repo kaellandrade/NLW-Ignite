03205308bcbc015fe9e484b71e434aced56e6ab2
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2022-10-16 13:19:32 -0300

    Utilizando toastify

--- sports/web/src/App.tsx
+++ sports/web/src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-import {AuthProvider} from './context/auth';
+import { AuthProvider } from './context/auth';
 import { Rotas } from './routes';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 	return (
 		<AuthProvider>
 			<Router>
+				<ToastContainer autoClose={8000} />
 				<Rotas />
 			</Router>
 		</AuthProvider>
