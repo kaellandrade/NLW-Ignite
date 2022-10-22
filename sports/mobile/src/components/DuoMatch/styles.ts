0475a43da0469054ec8bd21145d5eb0389069d11
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2022-10-22 12:21:16 -0300

    Adiconando estilos

---
+++ sports/mobile/src/components/DuoMatch/styles.ts
@@ -0,0 +1,44 @@
+import {StyleSheet} from "react-native";
+import {THEME} from "../../theme";
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: THEME.COLORS.OVERLAY
+	},
+	closeIcon: {
+		alignSelf: 'flex-end',
+		margin: 16
+	},
+	content: {
+		width: 311,
+		backgroundColor: THEME.COLORS.SHAPE,
+		borderRadius: 8,
+		alignItems: "center",
+	},
+	discordButton: {
+		width: 231,
+		height:48,
+		backgroundColor: THEME.COLORS.BACKGROUND_900,
+		justifyContent:'center',
+		alignItems:'center',
+		borderRadius:4,
+		marginBottom: 32
+	},
+	discord: {
+		color: THEME.COLORS.TEXT,
+		fontSize: THEME.FONT_SIZE.MD,
+		fontFamily: THEME.FONT_FAMILY.REGULAR,
+	},
+	label: {
+		color: THEME.COLORS.TEXT,
+		fontSize: THEME.FONT_SIZE.MD,
+		fontFamily: THEME.FONT_FAMILY.SEMI_BOLD,
+		marginTop: 24,
+		marginBottom: 24
+	}
+})
+
+export default styles;
--- sports/mobile/src/screens/Home/styles.ts
+++ sports/mobile/src/screens/Home/styles.ts
@@ -1,19 +1,15 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    alignItems:'center',
-  },
-  
-  logo:{
-    width:214,
-    height:120,
-    marginBottom : 25,
-    marginTop:74,
-  },
-  contentList:{
-    paddingLeft:32,
-    paddingRight:66
-  }
+	container: {
+		flex: .9,
+		alignItems: 'center',
+	},
+
+	logo: {
+		width: 214,
+		height: 120,
+		marginBottom: 25,
+		marginTop: 74,
+	},
 });
