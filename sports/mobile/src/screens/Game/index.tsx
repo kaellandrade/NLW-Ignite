3d232861b88fca92eba1c056e14d57e09d0d58e8
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2022-10-22 12:21:40 -0300

    Ajustando estilos home

--- sports/mobile/src/screens/Game/index.tsx
+++ sports/mobile/src/screens/Game/index.tsx
@@ -1,84 +1,93 @@
-import { useNavigation, useRoute } from "@react-navigation/native";
+import {useNavigation, useRoute} from "@react-navigation/native";
 import {
-  FlatList,
-  Image,
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-  View,
+	FlatList,
+	Image,
+	SafeAreaView,
+	Text,
+	TouchableOpacity,
+	View,
 } from "react-native";
-import { Entypo } from "@expo/vector-icons";
+import {Entypo} from "@expo/vector-icons";
 
-import { Gameparams } from "../../@types/navigation";
-import { Background } from "../../components/Background";
+import {Gameparams} from "../../@types/navigation";
+import {Background} from "../../components/Background";
 import logoImg from "../../assets/logo-nlw-esports.png";
 
-import { styles } from "./styles";
-import { THEME } from "../../theme";
-import { Heading } from "../../components/Heading";
-import { DuoCard, DuoCardPros } from "../../components/DuoCard";
-import { useEffect, useState } from "react";
+import {styles} from "./styles";
+import {THEME} from "../../theme";
+import {Heading} from "../../components/Heading";
+import {DuoCard, DuoCardPros} from "../../components/DuoCard";
+import {useEffect, useState} from "react";
 import API from "../../../api/api";
+import {DuoMatch} from "../../components/DuoMatch";
 
 export function Game() {
-  const [duos, setDuos] = useState<DuoCardPros[]>([]);
+	const [duos, setDuos] = useState<DuoCardPros[]>([]);
+	const [discordDuoSelected, setDiscordDuoSelected] = useState<string>('');
 
-  const route = useRoute();
-  const game = route.params as Gameparams;
-  const navigation = useNavigation();
+	const route = useRoute();
+	const game = route.params as Gameparams;
+	const navigation = useNavigation();
 
-  function handleGoBack() {
-    navigation.goBack();
-  }
+	function handleGoBack() {
+		navigation.goBack();
+	}
 
-  useEffect(function () {
-    const getAds = async () => {
-      const ads = await API.get(`/games/${game.id}/ads`);
-      setDuos(ads.data);
-    };
-    getAds();
-  }, []);
+	async function getDiscordUser(adsId: string) {
+		const resp = await API.get(`/ads/${adsId}/discord`);
+		setDiscordDuoSelected(resp.data.discord);
+	}
 
-  return (
-    <Background>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.header}>
-          <TouchableOpacity onPress={handleGoBack}>
-            <Entypo
-              name="chevron-thin-left"
-              color={THEME.COLORS.CAPTION_300}
-              size={20}
-            />
-          </TouchableOpacity>
-          <Image source={logoImg} style={styles.logo} />
-          <View style={styles.right} />
-        </View>
-        <Image
-          resizeMode="cover"
-          source={{ uri: game.bannerUrl }}
-          style={styles.cover}
-        />
-        <Heading title={game.title} subtitle="Conecte-se e comece a jogar!" />
+	useEffect(function () {
+		const getAds = async () => {
+			const ads = await API.get(`/games/${game.id}/ads`);
+			setDuos(ads.data);
+		};
+		getAds();
+	}, []);
 
-        <FlatList
-          data={duos}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <DuoCard onConnect={(_) => {}} data={item} />
-          )}
-          horizontal
-          contentContainerStyle={[
-            duos.length > 0 ? styles.contentList : styles.emptyListContent,
-          ]}
-          style={styles.containerList}
-          showsHorizontalScrollIndicator={false}
-          ListEmptyComponent={() => (
-            <Text style={styles.emptyList}>
-              Não há anúcios publicados ainda.
-            </Text>
-          )}
-        />
-      </SafeAreaView>
-    </Background>
-  );
+	return (
+		<Background>
+			<SafeAreaView style={styles.container}>
+				<View style={styles.header}>
+					<TouchableOpacity onPress={handleGoBack}>
+						<Entypo
+							name="chevron-thin-left"
+							color={THEME.COLORS.CAPTION_300}
+							size={20}
+						/>
+					</TouchableOpacity>
+					<Image source={logoImg} style={styles.logo}/>
+					<View style={styles.right}/>
+				</View>
+				<Image
+					resizeMode="cover"
+					source={{uri: game.bannerUrl}}
+					style={styles.cover}
+				/>
+				<Heading title={game.title} subtitle="Conecte-se e comece a jogar!"/>
+
+				<FlatList
+					data={duos}
+					keyExtractor={(item) => item.id}
+					renderItem={({item}) => (
+						<DuoCard onConnect={_ => getDiscordUser(item.id)} data={item}/>
+					)}
+					horizontal
+					contentContainerStyle={[
+						duos.length > 0 ? styles.contentList : styles.emptyListContent,
+					]}
+					style={styles.containerList}
+					showsHorizontalScrollIndicator={false}
+					ListEmptyComponent={() => (
+						<Text style={styles.emptyList}>
+							Não há anúncios publicados ainda.
+						</Text>
+					)}
+				/>
+				<DuoMatch onClose={() => setDiscordDuoSelected('')} discord={discordDuoSelected}
+						  visible={discordDuoSelected.length > 0}/>
+			</SafeAreaView>
+		</Background>
+	);
 }
